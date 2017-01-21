b4a4e3efa72cf6af850853ca42e97f53412c4033
Amninder-Chahal <amninderchahhal@gmail.com>
2017-01-21 17:08:07 -0500

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Play Rock Paper Scissors</title>
+    <link href="Style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div id="Content">
+        <header><p>Rock Paper Scissors</p></header>
+    <form id="form1" runat="server">
+        <p id="heading">
+            To start playing, choose an option below:
+        </p>
+        <p>
+            <asp:ImageButton class="img" ID="ImageButton1" runat="server" ImageUrl="~/Images/Rock.jpg" OnClick="ImageButton1_Click" />
+            <asp:ImageButton class="img" ID="ImageButton2" runat="server" ImageUrl="~/Images/Paper.jpg" OnClick="ImageButton2_Click" />
+            <asp:ImageButton class="img" ID="ImageButton3" runat="server" ImageUrl="~/Images/Scissors.jpg" OnClick="ImageButton3_Click" />
+        </p>
+        <p>
+             <asp:Label ID="Label3" runat="server"></asp:Label><br />
+            <asp:Label ID="Label1" runat="server"></asp:Label><br />
+            <asp:Label ID="Label2" runat="server"></asp:Label>
+        </p>
+        <table><tr>
+            <td class="tableText">
+            Total games played: </td>
+            <td><asp:Label ID="Label4" runat="server" Text="0"></asp:Label>
+            </td>
+            </tr><tr>
+            <td class="tableText">
+            You Won: </td>
+            <td><asp:Label ID="Label5" runat="server" Text="0"></asp:Label>
+            </td>
+           </tr><tr>
+            <td class="tableText">
+            You Lost: </td>
+            <td><asp:Label ID="Label6" runat="server" Text="0"></asp:Label>
+            </td>
+            </tr>
+         </table><br />
+        <asp:Button ID="Button1" runat="server" Text="Restart Game" OnClick="Button1_Click" />
+    </form>
+    </div>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private static int user_Choice, com_Choice, temp, total, wins, loses;
+    private void resetGame()
+    {
+        total = 0;
+        wins = 0;
+        loses = 0;
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Game()
+    {
+        int random = new Random().Next(1, 4);
+        int? tempRandom=null;
+        if (random == user_Choice|| random == tempRandom)
+        {
+                random = new Random().Next(1, 4);
+        }
+        else
+        {
+            com_Choice = random;
+            tempRandom = random;
+        }
+        
+        switch (user_Choice)
+        {
+            case 1:
+                if (com_Choice == 2)
+                {
+                    Label1.Text = "Computer chose Paper";
+                    temp = 1;
+                }
+                else
+                {
+                    Label1.Text = "Computer chose Scissors";
+                    temp = 2;
+                }
+                break;
+            case 2:
+                if (com_Choice == 3)
+                {
+                    Label1.Text = "Computer chose Scissors";
+                    temp = 1;
+                }
+                else
+                {
+                    Label1.Text = "Computer chose Rock";
+                    temp = 2;
+                }
+                break;
+            case 3:
+                if (com_Choice == 1)
+                {
+                    Label1.Text = "Computer chose Rock";
+                    temp = 1;
+                }
+                else
+                {
+                    Label1.Text = "Computer chose Paper";
+                    temp = 2;
+                }
+                break;
+                
+        }
+        if (temp == 1)
+        {
+            Label2.Text = "The computer won!";
+            total++;
+            loses++;
+        }
+        if (temp==2)
+        {
+            Label2.Text = "You won!";
+            wins++;
+            total++;
+        }
+    }
+    protected void gameCount()
+    {
+        Label4.Text = Convert.ToString(total);
+        Label5.Text = Convert.ToString(wins);
+        Label6.Text = Convert.ToString(loses);
+    }
+    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    {
+        user_Choice = 1;
+        Game();
+        gameCount();
+        Label3.Text = "You chose Rock";
+    }
+
+    protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+    {
+        user_Choice = 2;
+        Game();
+        Label3.Text = "You chose Paper";
+        gameCount();
+    }
+
+    protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
+    {
+        user_Choice = 3;
+        Game();
+        Label3.Text = "You chose Scissors";
+        gameCount();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        resetGame();
+        gameCount();
+        Label1.Text = "";
+        Label2.Text = "";
+        Label3.Text = "";
+    }
+}
